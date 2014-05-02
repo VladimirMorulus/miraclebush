8deddba7bab4444002877ee329560ad63e57279f
Morulus <vladimirmorulus@gmail.com>
2014-05-02 05:38:03 +0300

    depatchurl

--- dist/required/urlhash/urlhash.mb.1.0.js
+++ dist/required/urlhash/urlhash.mb.1.0.js
@@ -17,5 +17,15 @@ $bush.url = {
 			
 			callback(location.pathname, location);
 		});	
+	},
+	depatchurl : function(link, prefix, sufix) {
+		var link = link;
+		var prefix = prefix || '';
+		var sufix = sufix || '';
+		var pathname = link;
+		if (pathname.substring(0, prefix.length)==prefix) pathname = pathname.substring(prefix.length, pathname.length);
+		
+		if (pathname.substring(pathname.length-sufix.length)==sufix) pathname = pathname.substring(0, pathname.length-sufix.length);
+		return pathname;
 	}
 };
